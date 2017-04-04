d18bea6c8bec15458b0bde2d1ba51d2ab1d38805
Junmo Kang <keaton@starkcorp.co.kr>
2017-04-04 11:22:47 +0900

    레이어 팝업 등장시 자동스크롤 방지, 텍스트 위치 수정 Todo : css 스타일 정리, 텍스트 스타일을 한꺼번에 모아놓는 것은 어떨까? 태그별 스타일 뒤섞여서 찾기 어려움.

--- js/stark.js
+++ js/stark.js
@@ -23,32 +23,27 @@ $(document).ready(function() {
 
 function layer_open(el){
 
-		var temp = $('#' + el);		//레이어의 id를 temp변수에 저장
-		var bg = temp.prev().hasClass('bg');	//dimmed 레이어를 감지하기 위한 boolean 변수
+		var temp = $('#' + el);		
+		var bg = temp.prev().hasClass('bg');
 
 		if(bg){
 			$('.layer').fadeIn();
 		}else{
-			temp.fadeIn();	//bg 클래스가 없으면 일반레이어로 실행한다.
+			temp.fadeIn();
 		}
 
-		// 화면의 중앙에 레이어를 띄운다.
-		// if (temp.outerHeight() < $(document).height() ) {
-		// 	temp.css('margin-top', '-'+temp.outerHeight()/2+'px');
-		// } else {
-		// 	temp.css('top', '0px');
-		// }
-		// if (temp.outerWidth() < $(document).width() ) {
-		// 	temp.css('margin-left', '-'+temp.outerWidth()/2+'px');
-		// } else {
-		// 	temp.css('left', '0px');
-		// }
+		
+		$.fn.fullpage.setMouseWheelScrolling(false);
+		$.fn.fullpage.setAllowScrolling(false);
 
+		
 		temp.find('.closeBtn').click(function(e){
+			$.fn.fullpage.setMouseWheelScrolling(true);
+			$.fn.fullpage.setAllowScrolling(true);
 			if(bg){
 				$('.layer').fadeOut();
 			}else{
-				temp.fadeOut();		//'닫기'버튼을 클릭하면 레이어가 사라진다.
+				temp.fadeOut();
 			}
 			e.preventDefault();
 		});
